97600753998b8229f43b56de219ef855e198b70a
admin-asb <asb210297@gmail.com>
2024-01-29 18:00:36 +0600

    Second commit

--- main.js
+++ main.js
@@ -1,27 +1,196 @@
-const data = [
-  {
-    lot: "tb",
-    title: "TeleBingo",
-    text: "Самая популярная лотерея в Казахстане! Еженедельные розыгрыши крупных сумм Bingo и огромного SuperBingo, делают «TeleBingo» по-настоящему любимой лотереей, которую ждут, участвуют и смотрят всей семьей.",
+const data = {
+  tb: {
+    title: "«TeleBingo»",
+    text: "Самая популярная лотерея в&nbsp;Казахстане! Еженедельные розыгрыши крупных сумм Bingo и&nbsp;огромного SuperBingo, делают «TeleBingo» по-настоящему любимой лотереей, которую ждут, участвуют и&nbsp;смотрят всей семьей.",
+    linkBtn: "https://sz.kz/game?gameid=1082",
+    linkRules: "https://static.sz.kz/docs/terms_telebingo.pdf",
   },
-  {
-    lot: "bt",
-    title: "BingoToy",
-    text: "Самая популярная лотерея в Казахстане! Еженедельные розыгрыши крупных сумм Bingo и огромного SuperBingo, делают «BingoToy» по-настоящему любимой лотереей, которую ждут, участвуют и смотрят всей семьей.",
+  bt: {
+    title: "«BingoToy»",
+    text: "Праздничный тираж лотереи «TeleBingo»! Проводится вместо обычного тиража. Даты проведения в&nbsp;2023&nbsp;году: 26&nbsp;марта, 25&nbsp;июня, 24&nbsp;сентября и&nbsp;31&nbsp;декабря. Купите лотонабор <b>Мега розыгрыш</b> и&nbsp;участвуйте в&nbsp;розыгрыше <b>мега&nbsp;подарков.</b>",
+    linkRules: "https://static.sz.kz/content/gamesCatalog/docs/bingotoi-ru.pdf",
   },
-  {
-    lot: "se",
-    title: "Something Else",
-    text: "Самая популярная лотерея в Казахстане! Еженедельные розыгрыши крупных сумм Bingo и огромного SuperBingo, делают «Something Else» по-настоящему любимой лотереей, которую ждут, участвуют и смотрят всей семьей.",
+  l649: {
+    title: "«LOTO 6/49»",
+    text: "Одна из самых популярных тиражных лотерей в&nbsp;Казахстане. Суперприз от&nbsp;30&nbsp;миллионов&nbsp;тенге. Тиражи проходят каждую неделю по&nbsp; понедельникам, средам и&nbsp;пятницам.",
+    linkBtn: "https://sz.kz/game?gameid=1079",
+    linkRules: "https://static.sz.kz/docs/6_49-2-ru.pdf",
   },
-  {
-    lot: "m",
-    title: "More",
-    text: "Самая популярная лотерея в Казахстане! Еженедельные розыгрыши крупных сумм Bingo и огромного SuperBingo, делают «More» по-настоящему любимой лотереей, которую ждут, участвуют и смотрят всей семьей.",
+  l536: {
+    title: "«5/36»",
+    text: "Простая и доступная тиражная лотерея. Рекордсмен по разыгранным Джекпотам. Минимальный гарантированный Джекпот, начинается от&nbsp;3&nbsp;миллионов&nbsp;тенге. Тиражи&nbsp;– ежедневно в&nbsp;22:00.",
+    linkBtn: "https://sz.kz/game?gameid=1084",
+    linkRules: "https://static.sz.kz/docs/5_36_2023_rus.pdf",
   },
-];
+  keno: {
+    title: "«KENO»",
+    text: "Именно на деньги от «KENO» была построена Великая китайская стена. Постройте и&nbsp;вы, свою Великую стену Удачи вместе с&nbsp;«KENO»! Ежедневно, вечером в&nbsp;22:00.",
+    linkBtn: "https://sz.kz/game?gameid=1080",
+    linkRules: "https://static.sz.kz/docs/keno_ru.pdf",
+  },
+  keno2: {
+    title: "«KENO2»",
+    text: "Лотерея «KENO2» для тех, кто не&nbsp;хочет ждать до&nbsp;вечера. Розыгрыш каждый день, в&nbsp;14:30. Главный приз&nbsp;– до&nbsp;400&nbsp;миллионов&nbsp;тенге. «KENO2» может стать вашей Великой стеной Удачи!",
+    linkBtn: "https://sz.kz/game?gameid=1083",
+    linkRules: "https://static.sz.kz/docs/keno2_ru.pdf",
+  },
+  sl: {
+    title: "«SuperLoto»",
+    text: "Новая тиражная лотерея «SuperLoto»&nbsp;– это отличный баланс между Суперпризом и&nbsp;шансом на&nbsp;победу. Тиражи ежедневно, в&nbsp;19:00. Билеты от&nbsp;300&nbsp;тенге. Каждый четвёртый билет выигрышный.",
+    linkBtn: "https://sz.kz/game?gameid=1043",
+    linkRules:
+      "https://static.sz.kz/landings/superLotoKatalog/docs/RulesSuperLoto_ru.pdf",
+  },
+  l777: {
+    title: "«777»",
+    text: "Ежедневная тиражная лотерея, полюбившаяся за простоту и легкость участия. Стоимость билета всего 200&nbsp;тенге, а&nbsp;Главный&nbsp;приз&nbsp;- 100&nbsp;000&nbsp;тенге!",
+    linkBtn: "https://sz.kz/game?gameid=1081",
+    linkRules: "https://static.sz.kz/docs/777_ru.pdf",
+  },
+  l820: {
+    title: "«8/20»",
+    text: "Новая числовая лотерея «8/20». Играть&nbsp;- просто, выигрывать&nbsp;- легко! Стартовый Главный приз от&nbsp;1&nbsp;000&nbsp;000&nbsp;тенге. Тиражи ежедневно, в&nbsp;19:00. Билеты от&nbsp;300&nbsp;тенге. Каждый четвёртый билет выигрышный.",
+    linkBtn: "https://sz.kz/game?gameid=1041",
+    linkRules: "https://static.sz.kz/content/gamesCatalog/docs/820-ru.pdf",
+  },
+  l420: {
+    title: "«4/20»",
+    text: "Тиражная лотерея «4/20»&nbsp;– это числовая лотерея с&nbsp;высоким шансом на&nbsp;победу. Главный приз от&nbsp;5&nbsp;000&nbsp;000&nbsp;тенге. Тираж ежедневно, в&nbsp;20:00. Стоимость билета&nbsp;- от&nbsp;300&nbsp;тенге. Выигрывает каждый третий билет!",
+    linkBtn: "https://sz.kz/game?gameid=1042",
+    linkRules: "https://static.sz.kz/content/gamesCatalog/docs/420-ru.pdf",
+  },
+  bolwe: {
+    title: "«Больше Меньше»",
+    text: 'Перед вами числа в диапазоне от&nbsp;2&nbsp;до&nbsp;14. Нужно выбрать "Больше" или "Меньше" будет следующее выпавшее число. Главный приз 122&nbsp;200&nbsp;тенге.',
+    linkBtn: "https://sz.kz/game?gameid=1051",
+    linkRules:
+      "https://static.sz.kz/landings/bolsheMensheKatalog/docs/rulesBolweMenweRu.pdf",
+  },
+  l1224: {
+    title: "«12/24 Express»",
+    text: "Простота - залог крупных выигрышей! Угадай 12&nbsp;цифр из&nbsp;24-х, и&nbsp;Суперприз 60&nbsp;миллионов возможно станет твоим!",
+    linkBtn: "https://sz.kz/game?gameid=1053",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/express-1224-ru.pdf",
+  },
+  le420: {
+    title: "«4/20 Express»",
+    text: "Угадай 4&nbsp;цифры из&nbsp;20-ти, и&nbsp;выигрывай до&nbsp;50&nbsp;миллионов тенге каждые 5&nbsp;минут!",
+    linkBtn: "https://sz.kz/game?gameid=1044",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/express-420-ru.pdf",
+  },
+  bongo12: {
+    title: "«Bongo12»",
+    text: "«Bongo12» - фаворит быстротиражных лотерей. Удобный интерфейс и&nbsp;высокий шанс выиграть Главный приз&nbsp;- это то, что нравится всем участникам.",
+    linkBtn: "https://sz.kz/game?gameid=1049",
+    linkRules:
+      "https://static.sz.kz/landings/bongo12Katalog/docs/bongo-12-rules-ru.pdf",
+  },
+  le536: {
+    title: "«5/36 Express»",
+    text: "Чтобы стать участником, отметьте в&nbsp;билете не&nbsp;менее 5&nbsp;чисел в&nbsp;диапазоне от&nbsp;1&nbsp;до&nbsp;36. Главный приз лотереи 5&nbsp;000&nbsp;000&nbsp;тенге!",
+    linkBtn: "https://sz.kz/game?gameid=1078",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/536_ExpressRU.pdf",
+  },
+  bingom: {
+    title: "«BingoMingo»",
+    text: "Лотерея с самой высокой вероятностью выигрыша. Призовой Фонд&nbsp;– 94,74%. Победителей всегда много!",
+    linkBtn: "https://sz.kz/game?gameid=1048",
+    linkRules:
+      "https://static.sz.kz/landings/bingoMingoKatalog/docs/rulesBingoMingo.pdf",
+  },
+  ekeno: {
+    title: "«KENO Express»",
+    text: "Лотерея, знакомая каждому, доступна каждые 5&nbsp;минут. Участвуйте и&nbsp;выигрывайте!",
+    linkBtn: "https://sz.kz/game?gameid=1050",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/keno-express.pdf",
+  },
+  rapido: {
+    title: "«Rapido»",
+    text: "Захватывающая и&nbsp;быстрая лотерея! Угадай 8&nbsp;чисел в&nbsp;одном и&nbsp;1&nbsp;число во&nbsp;втором поле, и&nbsp;поборись за&nbsp;джекпот!",
+    linkBtn: "https://sz.kz/game?gameid=1052",
+    linkRules: "https://static.sz.kz/content/gamesCatalog/docs/rapido-ru.pdf",
+  },
+  penalty: {
+    title: "«Penalty»",
+    text: "Первая быстротиражная спортивная лотерея. Высокий шанс выигрыша. Тираж каждые 2&nbsp;минуты.",
+    linkBtn: "https://sz.kz/game?gameid=1085",
+    linkRules: "https://static.sz.kz/content/gamesCatalog/docs/penalty-ru.pdf",
+  },
+  le649: {
+    title: "«6/49 Express»",
+    text: "Главный приз – 14&nbsp;миллионов&nbsp;тенге. Выберите от&nbsp;6&nbsp;до&nbsp;12&nbsp;чисел и&nbsp;ждите результаты тиража. Тираж проводится каждые 5&nbsp;минут.",
+    linkBtn: "https://sz.kz/game?gameid=1045",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/express-649-ru.pdf",
+  },
+  e30l: {
+    title: "«30Лото»",
+    text: "В быстротиражной лотерее «30Лото», каждый 2-й билет выигрышный. Испытайте удачу выиграть до&nbsp;4&nbsp;008&nbsp;000&nbsp;тенге!",
+    linkBtn: "https://sz.kz/game?gameid=1089",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/30Loto-Express.pdf",
+  },
+  lemon: {
+    title: "«Crazy Lemon»",
+    text: "Моментальная лотерея «Crazy Lemon» даёт вам шанс выиграть 100&nbsp;миллионов! Вы можете выбрать 8&nbsp;или&nbsp;16&nbsp;билетов одновременно. Соберите из&nbsp;букв слова «Сәтті Жұлдыз» и&nbsp;получите гарантированный приз.",
+    linkBtn: "https://sz.kz/game?gameid=1162",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/moment/lemon-ru.pdf",
+  },
+  chukcha: {
+    title: "«Chukcha»",
+    text: "Приглашаем вас в&nbsp;увлекательное лотерейное путешествие по&nbsp;Крайнему Северу. Отличная графика и&nbsp;привлекательные персонажи подарят вам массу положительных эмоций. Главный приз 10&nbsp;000&nbsp;000&nbsp;тенге, удачной рыбалки!",
+    linkBtn: "https://sz.kz/game?gameid=1177",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/moment/chukcha-ru.pdf",
+  },
+  taga: {
+    title: "«Таға»",
+    text: "Подкова, висящая в доме, всегда приносила удачу кочевникам и&nbsp;их&nbsp;потомкам. Участвуйте в&nbsp;лотерее «Таға» и&nbsp;пусть вам тоже улыбнется удача!",
+    linkBtn: "/gde-kupit-lotereyu",
+    linkRules: "https://static.sz.kz/docs/new/Taga-ru.pdf",
+  },
 
-const popup = document.querySelector(".popup-content");
+  kp: {
+    title: "«Құттықтаймыз! Поздравляю!»",
+    text: "Первая беспроигрышная лотерея. 5&nbsp;полей по&nbsp;цене&nbsp;4. Приз в&nbsp;каждом билете. Главный приз&nbsp;– 5&nbsp;миллионов&nbsp;тенге.",
+    linkBtn: "/gde-kupit-lotereyu",
+    linkRules: "https://static.sz.kz/docs/new/Quttiktaimiz-ru.pdf",
+  },
+  lq: {
+    title: "«Lucky Queen»",
+    text: "Королева удачи сделает вас богаче! Только в&nbsp;лотерее «Lucky Queen» у&nbsp;вас есть уникальный шанс выиграть три игры в&nbsp;одном билете.",
+    linkBtn: "https://sz.kz/game?gameid=1164",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/moment/luckyqueen-ru.pdf",
+  },
+  moneyb: {
+    title: "«MoneyБар»",
+    text: "Участвуйте в самой захватывающей мгновенной лотерее «MoneyБар» и&nbsp;выиграйте Главный&nbsp;приз в&nbsp;размере 500&nbsp;000&nbsp;тенге. Пираты и&nbsp;сокровища, солнце и&nbsp;попугаи все это и&nbsp;многое другое ждут вас!",
+    linkBtn: "https://sz.kz/game?gameid=1173",
+    linkRules: "https://static.sz.kz/docs/moneyBar__ru.pdf",
+  },
+  fb: {
+    title: "«ФрутоBOOM»",
+    text: "Сочные фрукты помогут вам зарядится витаминами и&nbsp;быть всегда в&nbsp;тонусе. Наша самая фруктовая лотерея подарит вам отличное настроение и&nbsp;даст возможность выиграть до&nbsp;20&nbsp;миллионов&nbsp;тенге.",
+    linkBtn: "https://sz.kz/game?gameid=1016",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/moment/frutoboom-ru.pdf",
+  },
+  fni: {
+    title: "«Fruit−N−Ice»",
+    text: "Приглашаем вас в фруктовое приключение. Вас ждут свежие фрукты в&nbsp;кубиках льда. Собирайте специальные символы и&nbsp;участвуйте в&nbsp;бонусных играх. А&nbsp;пять сочных лимонов подряд могут подарить вам 100&nbsp;миллионов&nbsp;тенге!",
+    linkBtn: "https://sz.kz/game?gameid=1170",
+    linkRules:
+      "https://static.sz.kz/content/gamesCatalog/docs/moment/fruitnice-ru.pdf",
+  },
+};
+
+const popup = document.querySelector(".popup-inner");
 const overlay = document.querySelector(".overlay");
 const btnClosePopup = document.querySelector(".btn--close-popup");
 const btnsOpenModal = document.querySelector(".btn--show-modal");
@@ -31,9 +200,8 @@ const body = document.querySelector("body");
 // Modal window
 
 const openModal = function () {
-  console.log("Works");
-  //   popup.classList.remove("hidden");
-  //   overlay.classList.remove("hidden");
+  popup.classList.remove("hidden");
+  overlay.classList.remove("hidden");
   body.style.overflowY = "hidden";
   if (header) {
     header.style.zIndex = "1";
@@ -42,12 +210,11 @@ const openModal = function () {
 
 const closePopup = function () {
   popup.classList.add("hidden");
-  //   overlay.classList.add("hidden");
+  overlay.classList.add("hidden");
   body.style.overflowY = "initial";
 };
 
 btnsOpenModal.addEventListener("click", () => {
-  console.log("Button clicked!");
   openModal();
 });
 
@@ -65,58 +232,34 @@ document.addEventListener("keydown", function (e) {
 
 const tabs = document.querySelectorAll(".grid-item");
 const tabsContainer = document.querySelectorAll(".grid-container");
-const tabsContent = document.querySelectorAll(".operations__content");
+const popupTitle = document.querySelector(".popup__description h3");
+const popupText = document.querySelector(".popup__description p");
+const btnBuy = document.querySelector(".buy-btn");
+const btnRules = document.querySelector(".rules-btn");
 
 tabsContainer.forEach(container => {
   container.addEventListener("click", function (e) {
-    const mod = document.querySelector(".mod");
-    mod.innerHTML = "";
-
     const clicked = e.target.closest(".grid-item");
+    console.log(clicked);
 
-    if (!clicked) return;
+    if (!clicked || !clicked.dataset.tab) return;
 
     tabs.forEach(t => t.classList.remove("btn--show-modal"));
-    tabsContent.forEach(c => c.classList.remove("operations__content--active"));
-    // popup.classList.add("hidden");
 
     clicked.classList.add("btn--show-modal");
 
-    document
-      .querySelector(`.operations__content--${clicked.dataset.tab}`)
-      .classList.add("operations__content--active");
+    const tabData = data[clicked.dataset.tab];
+
+    if (tabData) {
+      popupTitle.innerText = tabData.title;
+      popupText.innerHTML = tabData.text;
+      if (tabData.linkBtn) {
+        btnBuy.setAttribute("href", tabData.linkBtn);
+      } else {
+        btnBuy.classList.add("hidden");
+      }
+      btnRules.setAttribute("href", tabData.linkRules);
+      openModal();
+    }
   });
 });
-
-// const filteredData = data.filter(d => clicked.dataset.tab === d.lot);
-
-// filteredData.forEach(d => {
-//   const html = `
-//       <div class="popup-content popup__content-${d.lot}">
-//         <div class="popup-inner">
-//           <button class="btn--close-popup">&times;</button>
-//           <div class="popup__pic">
-//             <picture>
-//               <img src="./img/classic/tb-pp.png" srcset="./img/classic/tb-ppx2.png 2x" alt=" " />
-//             </picture>
-//           </div>
-//           <div class="popup__description">
-//             <h3>${d.title}</h3>
-//             <p>${d.text}</p>
-//             <a href="/profile?promocode=INDP10ALL">
-//               <button class="popup__btn">купить билет</button>
-//             </a>
-//             <a href="/profile?promocode=INDP10ALL">
-//               <button class="popup__btn popup__btn2">
-//                 <img src="./img/download-icon.svg" alt="" />
-//                 <p>Правила лотереи</p>
-//               </button>
-//             </a>
-//           </div>
-//         </div>
-//         <div class="overlay"></div>
-//       </div>
-//     `;
-
-//   mod.insertAdjacentHTML("beforeend", html);
-// });
